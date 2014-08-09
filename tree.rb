07ba8dfef41ae6b4126be1912bed767128480657
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-08 22:11:33 -0700

    add #search and driver code

--- tree.rb
+++ tree.rb
@@ -6,7 +6,7 @@ class Node
   def initialize(value = nil, left = nil, right = nil)
     @left = left
     @right = right
-    @value = valueend
+    @value = value
   end
 end
 
@@ -18,4 +18,21 @@ node3 = Node.new(3, node1, node4)
 node10 = Node.new(10, node7, node12)
 root = Node.new(5, node3, node10)
 
+def search(root, value)
+  if root.value == value
+    root
+  else
+    current_node = root
+    while current_node
+      return current_node if current_node.value == value
+      current_node = current_node.left if current_node.value > value
+      current_node = current_node.right if current_node.value < value
+    end
+    false
+  end
+end
+
+p search(root, 12).value == 12
+p search(root, 1).value == 1
+p search(root, 5).value == 5
 
