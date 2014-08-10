e5b118b0dcf9aea6f258862e12dae7a3a4d22859
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-10 16:49:10 -0700

    add #first_repeat

--- array.rb
+++ array.rb
@@ -1,1 +1,17 @@
 # Find first none repeating char
+# Use true false as hash value to indicate whether a char is a repeat or not
+# instead of using integers. CLEVER!
+
+def first_repeat(string)
+  lib = {}
+  string.each_char do |c|
+    if lib[c.to_sym] == nil
+      lib[c.to_sym] = false
+    elsif lib[c.to_sym] == false
+      lib[c.to_sym] = true
+    end
+  end
+end
+
+s = "teeter"
+first_repeat(s)
