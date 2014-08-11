64ed30fe989fd1c720cc3ca3cfffc9f06c5bc292
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-10 18:03:49 -0700

    add #delete_words

--- array.rb
+++ array.rb
@@ -14,7 +14,27 @@ def first_repeat(string)
   string.each_char do |c|
     return c if lib[c.to_sym] == false
   end
+  return false # if all chars are repeated
+end
+
+def delete_words(target, source)
+  libs = {}
+  processed_string = ""
+  source.each_char do |c|
+    libs[c.to_sym] = true
+  end
+  target.each_char do |c|
+    processed_string << c unless libs[c.to_sym] == true
+  end
+  processed_string
 end
 
 s = "teeter"
 p first_repeat(s) == "r"
+
+s = "total"
+p first_repeat(s) == "o"
+
+t = "Battle of the Vowels: Hawaii vs. Grozny"
+s = "aeiou"
+p delete_words(t, s) == "Bttl f th Vwls: Hw vs. Grzny"
