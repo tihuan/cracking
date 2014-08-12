47928b7192821689f5944fbcce3e075ec3f9bcfb
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-12 14:28:44 -0700

    add #quick_sort. It's in place quicksort so the place complexity is logn

--- sorting.rb
+++ sorting.rb
@@ -13,7 +13,46 @@ def selection_sort(array)
   array
 end
 
-array = [3,4,1,2,5,1,2,7,8,5,13,1,1,1,3,1]
-p selection_sort(array) == array.sort
+def quick_sort(array, lower = 0, upper = array.size - 1)
+  p "===entering quick sort==="
+  p "begin array: #{array}"
+  p "lower: #{lower}"
+  p "upper: #{upper}"
+  pivot_value = array[(lower + upper) / 2]
+  p "pivot_index: #{(lower + upper) / 2}, pivot_value: #{pivot_value}"
+  i = lower
+  j = upper
+
+  while i <= j
+    p "i: #{i}, j: #{j}"
+    while array[i] < pivot_value
+      i += 1
+    end
+
+    while array[j] > pivot_value
+      j -= 1
+    end
+
+    if i <= j
+      p "before swapping array: #{array}"
+      p "i: #{i} - value: #{array[i]}, j: #{j} - value: #{array[j]}"
+      array[i], array[j] = array[j], array[i] if i < j
+      i += 1
+      j -=1
+      p "while loop array: #{array}"
+    end
+    p "ending i: #{i}, j: #{j}"
+  end
+  quick_sort(array, lower, j) if lower < j
+  quick_sort(array, i, upper) if i < upper
+  p "After loop array: #{array}"
+  array
+end
+
+array = [1,10,2,9,3,8,4,7,5,6]
+# array = [1,6,2,9,3,8,4,7,5,10]
+# array = [1,6,2,5,3,8,4,7,9,10]
+# p selection_sort(array) == array.sort
+p quick_sort(array) == array.sort
 
 
