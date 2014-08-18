9e6cd816c2a50a4561e7b141cbe9bc31df0fea9f
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-17 20:41:36 -0700

    add #mergesort and #merge

--- sorting.rb
+++ sorting.rb
@@ -78,6 +78,30 @@ def insertion_sort(container)
   container
 end
 
+ # Mergesort: A stable divide-and-conquer sort that sorts small chunks of the container and then merges them together.
+# Returns an array of the sorted elements.
+# Requirements: Container should implement []
+# Time Complexity: О(n log n) average and worst-case
+# Space Complexity: О(n) auxiliary
+# Stable: Yes
+#
+# Algorithms::Sort.mergesort [5, 4, 3, 1, 2] => [1, 2, 3, 4, 5]
+def mergesort(container)
+  return container if container.size <= 1
+  mid = container.size / 2
+  left = container[0...mid]
+  right = container[mid...container.size]
+  merge(mergesort(left), mergesort(right))
+end
+
+def merge(left, right)
+  sorted = []
+  until left.empty? or right.empty?
+    left.first <= right.first ? sorted << left.shift : sorted << right.shift
+  end
+  sorted + left + right
+end
+
 array = [1,10,2,9,3,8,4,7,5,6]
 # array = [1,6,2,9,3,8,4,7,5,10]
 # array = [1,6,2,5,3,8,4,7,9,10]
